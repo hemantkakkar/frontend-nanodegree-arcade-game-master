d56f86dbc6c82328847e001272a2caf9e871e0af
Hemant Kakkar <hemantkakkar77@gmail.com>
2017-08-14 20:15:22 +0530

    Add files via upload
    Now working fine

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# frontend-nanodegree-arcade-game-master
-A simple game
+#Arcade game for Project#3 by Hemant Kakkar
+PlayFrogger at - https://github.com/hemantkakkar/Archade_game_clone
+
+##How to play:
+   * To play game,click on https://github.com/hemantkakkar/Archade_game_clone
+   * Dowload or clone the reposiyory.
+   * Click on index.html.   
+   * Use the arrow keys to move.
+   * You have five lives.
+   * Player must reach water to win the game without colliding with Enemy-bugs
+   * After each collision,the game will be reset
+   * Both Grass areas are safe zones.
+   * Have fun.
+
+##Resources I used to understand the process:
+   1. http://www.lostdecadegames.com/how-to-make-a-simple-html5-canvas-game/ - to understand the process of game construction
+   2. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math - to use some Math  functions for random movement
+   3. http://errietta.me/blog/move-an-image-around-with-arrow-keys/ - to understand the arrow key input process
+   4. http://devmag.org.za/2009/04/13/basic-collision-detection-in-2d-part-1/ - to understand the collision process
---
+++ js/app.js
@@ -0,0 +1,145 @@
+// Enemies our player must avoid
+"use strict";
+// possible vlues of Y axis locations for bugs
+var enemyYvalues = [50, 130, 210];
+
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+var Enemy = function () {
+    this.sprite = 'images/enemy-bug.png';
+    this.x = 0;
+
+    // randomly selected Y location for each bug
+    this.y = enemyYvalues[Math.floor(Math.random() * 3)];
+
+    // for random speed of the bugs
+    this.speed = Math.floor(100 + (Math.random() * 200));
+};
+
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+
+    // movement compatibility
+    this.x += this.speed * dt;
+
+    // if enemy moves out of the frame, following code changes enemy's speed and Y location (row)
+    if (this.x > 505) {
+        this.x = -101;
+        this.y = this.y + 83;
+        this.speed = Math.floor(100 + (Math.random() * 200));
+
+        // avoids enemy to allocate any Y location on grass rows
+        if (this.y > 226) {
+            this.y = 60;
+        }
+    }
+
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+
+// setting up player function properties
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.x = 200;
+    this.y = 400;
+	this.counter = 0;
+};
+
+Player.prototype.update = function() {
+     // moving player in the direction button is pressed
+    if (this.currentkey === 'left' && this.x !== 0) {
+        this.x = this.x - 101;
+    } else if (this.currentkey === 'right' && this.x != 400) {
+        this.x = this.x + 101;
+    } else if (this.currentkey === 'up') {
+        this.y = this.y - 83;
+    } else if (this.currentkey === 'down' && this.y != 400) {
+        this.y = this.y + 83;
+    }
+    // setting value of currentKey to null will give player a hop movement
+    // otherwise, player will continue to move in the one direction
+    // unless, change its direction of movement
+    // player will never come to a stop
+    this.currentkey = null;
+	this.checkCollision();
+
+    // reseting player location
+    if (this.y < 60) {
+        alert('You Won!\n' + '\n' + 'Resetting Game..');
+        this.reset();
+    }
+};
+
+
+// Draw the player on the screen, required method for game
+Player.prototype.render = function() {
+   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(key){
+    this.currentkey = key;
+};
+
+// resetting Players location
+Player.prototype.reset = function() {
+    this.x = 200;
+    this.y = 400;
+};
+
+Player.prototype.checkCollision = function() { 
+    // check for collision between enemy and player
+    // limited number of collisions allowed, else Game over
+	for(var i = 0; i<allEnemies.length; i++) {
+		var currentEnemy = allEnemies[i];
+
+    if (!(currentEnemy.y + 70 < this.y ||
+        currentEnemy.y > this.y + 70 ||
+        currentEnemy.x + 78 < this.x ||
+        currentEnemy.x > this.x + 78)) {
+            this.counter = this.counter + 1;
+            if(this.counter >= 5) {
+                alert('Game Over!');
+            }
+            this.reset();
+        }
+	}
+};
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+var totalEnemies = 4;
+var allEnemies = [];
+
+for (var i = 0; i < totalEnemies; i++) {
+    allEnemies.push(new Enemy());
+}
+
+var player = new Player();
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
